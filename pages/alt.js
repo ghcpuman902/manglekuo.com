fc151e961e246d3abeda93f057ad0c8110b9efb8
MangleKuo <ghcpuman902@gmail.com>
2021-10-26 10:41:11 +0800

    Added z-index manager system

--- pages/alt.js
+++ pages/alt.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import altStyles from '../styles/alt.module.css'
-import {useEffect, useState, useRef} from 'react'
+import {useEffect, useState, useRef, useReducer} from 'react'
 
                     // DESCRIPTION CAN BE THIS LONG DESCRIPTION CAN BE THIS LONG DESCRIPTION CAN BE THIS LONG DESCRIPTION CAN BE THIS LONG ///////////
 const description = "I'm Mangle Kuo, a web developer who has strong interest in design, photography, beer and city. Looking for a new job.";
@@ -23,8 +23,6 @@ function AnimationVideoPlayer() {
 }
 
 function FindMe() {
-
-
 
     const findMeeTextList = [
         "⇱ Find mee",
@@ -86,34 +84,6 @@ function FindMe() {
             <div className={`${altStyles.overlay} ${showOverlay?altStyles.open:altStyles.closed}`}>
                 <div className={altStyles.findMeContentWrapper}>
                     <div className={altStyles.findMeContent}>
-                        {/* <a target="_blank" rel="noopener" href="https://github.com/ghcpuman902">
-                            <svg className={altStyles.Twitter} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M21.543,7.10409c.01463.21158.01463.42316.01463.63668a13.915,13.915,0,0,1-14.01,14.01v-.0039A13.93944,13.93944,0,0,1,0,19.53943a10.01717,10.01717,0,0,0,1.172.07118,9.88733,9.88733,0,0,0,6.11529-2.11188,4.93012,4.93012,0,0,1-4.60012-3.41938,4.908,4.908,0,0,0,2.223-.08483A4.92434,4.92434,0,0,1,.96039,9.1682V9.10579A4.89257,4.89257,0,0,0,3.19512,9.722,4.93007,4.93007,0,0,1,1.67118,3.14748a13.97523,13.97523,0,0,0,10.148,5.14418,4.92855,4.92855,0,0,1,8.391-4.49092A9.88049,9.88049,0,0,0,23.337,2.60537a4.94232,4.94232,0,0,1-2.16453,2.72322A9.79284,9.79284,0,0,0,24,4.55345,10.00318,10.00318,0,0,1,21.543,7.10409Z"/></svg>
-                            Twitter @MangleKuo
-                        </a>
-                        <a target="_blank" rel="noopener" href="https://github.com/ghcpuman902">
-                            <svg className={altStyles.GitHub} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
-                            GitHub ghcpuman902
-                        </a>
-                        <a target="_blank" rel="noopener" href="https://www.instagram.com/ghcpuman902/">
-                            <svg className={altStyles.Instagram} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Personal Instagram</title><path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"/></svg>
-                            Instagram @ghcpuman902
-                        </a>
-                        <a target="_blank" rel="noopener" href="https://www.instagram.com/manglekuo/">
-                            <svg className={altStyles.Instagram} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Work Instagram</title><path d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"/></svg>
-                            Instagram @manglekuo
-                        </a>
-                        <a target="_blank" rel="noopener" href="https://www.behance.net/gallery/65814819/Collection-of-works">
-                            <svg className={altStyles.Behance} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Behance</title><path d="M0 4.4804V19.243h7.1554c.6613 0 1.3078-.0832 1.9297-.248.6288-.1654 1.1905-.4203 1.6792-.7661.485-.3431.8784-.788 1.1677-1.3369.2862-.542.4294-1.187.4294-1.9354 0-.9232-.219-1.7109-.6675-2.369-.446-.6542-1.1187-1.1139-2.0274-1.3746.6674-.3161 1.1658-.7227 1.506-1.2177.3371-.4967.5058-1.1174.5058-1.8607 0-.6873-.1127-1.2677-.3375-1.7318-.2306-.4709-.552-.8452-.9632-1.1266-.4176-.2808-.912-.4857-1.4912-.6085-.5827-.1261-1.22-.1872-1.9264-.1872zm15.6674.9903v1.4567h5.9844V5.4707zM3.2509 6.9947h3.0407c.2873 0 .5683.0204.8359.0731.2728.0466.508.134.716.2595.2096.1205.3754.293.501.5132.1208.2203.1806.5038.1806.8474 0 .6189-.1811 1.0702-.5551 1.3426-.3778.2775-.8543.4147-1.4304.4147H3.2509zm15.545 1.2564c-.819 0-1.5587.1462-2.2294.436-.6705.2904-1.2463.6875-1.7318 1.1915-.4846.5011-.8535 1.0986-1.12 1.7909-.2612.69-.3942 1.4366-.3942 2.236 0 .8268.1284 1.5891.3835 2.2786.258.6923.6198 1.2822 1.0856 1.781.478.4967 1.046.8784 1.726 1.1497.6806.269 1.4382.4048 2.2803.4048 1.208 0 2.2446-.2771 3.0949-.8326.8599-.5528 1.4902-1.471 1.9058-2.7574h-2.585c-.1.3307-.359.649-.784.9467-.4295.2988-.9417.4492-1.534.4492-.8233 0-1.4588-.2168-1.8985-.6462-.4412-.4294-.7267-1.2289-.7267-2.0742h7.713c.0552-.8291-.0122-1.6218-.2045-2.3797-.1938-.7601-.5033-1.4365-.9393-2.029-.4355-.5931-.9904-1.0667-1.667-1.4165-.6788-.3543-1.4703-.5288-2.3747-.5288zm-.0887 2.217c.7209 0 1.3126.2092 1.6612.5954.3503.389.6065.9432.6766 1.6915h-4.7766c.0136-.2085.058-.4444.1339-.7045.0749-.2668.2039-.5164.3933-.753.1905-.2326.4402-.431.744-.5896.3109-.1608.6986-.2397 1.1676-.2397zM3.251 12.664h3.5334c.6996 0 1.2682.1602 1.6948.4836.4259.328.6405.8685.6405 1.6292 0 .3885-.0632.7094-.1946.9566-.131.2495-.3106.4466-.528.5896-.2172.1491-.4753.2498-.7661.3137-.2862.0639-.5905.092-.9115.092H3.2509z"/></svg>
-                            Behance mangle-kuo
-                        </a>
-                        <a target="_blank" rel="noopener" href="https://www.flickr.com/photos/65271177@N06/albums">
-                            <svg className={altStyles.Flickr} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Flickr</title><path d="M5.334 6.666C2.3884 6.666 0 9.055 0 12c0 2.9456 2.3884 5.334 5.334 5.334 2.9456 0 5.332-2.3884 5.332-5.334 0-2.945-2.3864-5.334-5.332-5.334zm13.332 0c-2.9456 0-5.332 2.389-5.332 5.334 0 2.9456 2.3864 5.334 5.332 5.334C21.6116 17.334 24 14.9456 24 12c0-2.945-2.3884-5.334-5.334-5.334Z"/></svg>
-                            Flickr Mangle Kuo
-                        </a>
-                        <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/mangle-k-84198691/">
-                            <svg className={altStyles.LinkedIn} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
-                            LinkedIn Mangle Kuo
-                        </a> */}
                         <SocialLink link="https://twitter.com/manglekuo" type="Twitter" tag="@MangleKuo" />
                         <SocialLink link="https://github.com/ghcpuman902" type="GitHub" tag="ghcpuman902" />
                         <SocialLink link="https://www.instagram.com/ghcpuman902/" type="Instagram" tag="@ghcpuman902" />
@@ -122,7 +92,6 @@ function FindMe() {
                         <SocialLink link="https://www.flickr.com/photos/65271177@N06/albums" type="Flickr" tag="Mangle Kuo" />
                         <SocialLink link="https://www.linkedin.com/in/mangle-k-84198691/" type="LinkedIn" tag="Mangle Kuo" />
                         <SocialLink link="mailto:manglekuo@gmail.com" type="Email" tag="MangleKuo@gmail.com" />
-
                     </div>
                 </div>
             </div>
@@ -138,7 +107,163 @@ function SocialLink(props) {
     </a>);
 }
 
+
+
+
+
+// ========================================================================
+
+
+
+const findMeeTextList = [
+    "⇱ Find mee",
+    "⇲ Go babe",
+    "⇲ Go bass",
+    "⇲ Go base",
+    "⇲ Go bake",
+    "⇲ Go back"
+];
+const findMeeTextListRev = [
+    "⇱ Find mee",
+    "⇱ Funk mee",
+    "⇱ Find sea",
+    "⇱ Fcuk mee",
+    "⇱ Find wee",
+    "⇲ Go back"
+];
+
+const initialState = {
+    fMText: "⇱ Find mee",
+    zOrder: [
+            'FindmeeCover',
+            'Findmee',
+            'ThingsIdidCover',
+            'ThingsIdid',
+            'ThingsIwroteCover',
+            'ThingsIwrote',
+            'MangleKuo',
+        ],
+    isOpen:{
+        ThingsIdidCover: false,
+        ThingsIwroteCover: false,
+        FindmeeCover: false,
+    },
+
+};
+
+function reducer(state, action) {
+    console.log(action);
+    switch (action.type) {
+        case 'setFMText':
+        return {fMText: action.val,zOrder:state.zOrder,isOpen:state.isOpen};
+        case 'showOverlay':
+            let new_zOrder = [...state.zOrder];
+            let i1,i2;
+            switch (action.val) {
+                case 'ThingsIdidCover':
+                    i1 = new_zOrder.indexOf('ThingsIdid');
+                    i2 = new_zOrder.indexOf('ThingsIdidCover');
+                    if (i1 > -1) {new_zOrder.splice(i1, 1);}
+                    if (i2 > -1) {new_zOrder.splice(i2, 1);}
+                    new_zOrder.push('ThingsIdidCover');
+                    new_zOrder.push('ThingsIdid');
+                    return {
+                        fMText:state.fMText,
+                        zOrder:new_zOrder,
+                        isOpen:{
+                            ThingsIdidCover: true,
+                            ThingsIwroteCover: false,
+                            FindmeeCover: false,
+                        }
+                    };
+                case 'ThingsIwroteCover':
+                    i1 = new_zOrder.indexOf('ThingsIwrote');
+                    i2 = new_zOrder.indexOf('ThingsIwroteCover');
+                    if (i1 > -1) {new_zOrder.splice(i1, 1);}
+                    if (i2 > -1) {new_zOrder.splice(i2, 1);}
+                    new_zOrder.push('ThingsIwroteCover');
+                    new_zOrder.push('ThingsIwrote');
+                    return {
+                        fMText:state.fMText,
+                        zOrder:new_zOrder,
+                        isOpen:{
+                            ThingsIdidCover: false,
+                            ThingsIwroteCover: true,
+                            FindmeeCover: false,
+                        }
+                    };
+                case 'FindmeeCover':
+                    i1 = new_zOrder.indexOf('Findmee');
+                    i2 = new_zOrder.indexOf('FindmeeCover');
+                    if (i1 > -1) {new_zOrder.splice(i1, 1);}
+                    if (i2 > -1) {new_zOrder.splice(i2, 1);}
+                    new_zOrder.push('FindmeeCover');
+                    new_zOrder.push('Findmee');
+                    console.log(new_zOrder);
+                    return {
+                        fMText:state.fMText,
+                        zOrder:new_zOrder,
+                        isOpen:{
+                            ThingsIdidCover: false,
+                            ThingsIwroteCover: false,
+                            FindmeeCover: true,
+                        }
+                    };
+                case 'HideAll':
+                    return {fMText:state.fMText,zOrder:state.zOrder,isOpen:{
+                            ThingsIdidCover: false,
+                            ThingsIwroteCover: false,
+                            FindmeeCover: false,
+                        }
+                    };
+                default:
+                    throw new Error();
+            }
+            return 
+        default:
+            throw new Error();
+  }
+}
+
+
+
 export default function Alt() {
+    let fMInx = useRef(0);
+
+    function changeFindMeText(inc){
+        let last = 0; // timestamp of the last render() call
+        function render(now) {
+            // console.log(now,fMInx+inc);
+            if(fMInx.current+inc < 0 || fMInx.current+inc > findMeeTextList.length-1){
+    
+            }else{
+                // console.log("inside",(now - last));
+                if(!last || (now - last) >= (inc>0?120:200)) {
+                    console.log("inside inside",(now - last),fMInx.current+inc);
+                    last = now;
+                    fMInx.current = fMInx.current+inc;
+                    dispatch({type: 'setFMText', val: inc>0?findMeeTextList[fMInx.current]:findMeeTextListRev[fMInx.current]})
+                }
+                requestAnimationFrame(render);
+            }
+        }
+        requestAnimationFrame(render);
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    function handleFindmeeClick(){
+
+        if(fMInx.current == 0){
+            changeFindMeText(1);
+            dispatch({type: 'showOverlay', val: 'FindmeeCover'});
+        }
+        if(fMInx.current == findMeeTextList.length-1){
+            changeFindMeText(-1);
+            dispatch({type: 'showOverlay', val: 'HideAll'});
+        }
+        
+    }
 
   return (
     <>
@@ -151,11 +276,31 @@ export default function Alt() {
             <section className={altStyles.videoSection}>
                 <AnimationVideoPlayer />
             </section>
-            <section>
-                <div className={altStyles.topLeft}>Mangle Kuo</div>
-                <div className={altStyles.topRight}>Things I did</div>
-                <div className={altStyles.bottomLeft}>Things I wrote</div>
-                <FindMe />
+            <section className={altStyles.fixedTexts}>                
+                <div className={`${altStyles.MangleKuo} ${altStyles['z-'+state.zOrder.indexOf('MangleKuo')]}`}>Mangle Kuo</div>
+
+                <div className={`${altStyles.ThingsIdid} ${altStyles['z-'+state.zOrder.indexOf('ThingsIdid')]}`}>Things I did</div>
+                <div className={`${altStyles.ThingsIdidCover} ${altStyles['z-'+state.zOrder.indexOf('ThingsIdidCover')]}`}></div>
+
+                <div className={`${altStyles.ThingsIwrote} ${altStyles['z-'+state.zOrder.indexOf('ThingsIwrote')]}`}>Things I wrote</div>
+                <div className={`${altStyles.ThingsIwroteCover} ${altStyles['z-'+state.zOrder.indexOf('ThingsIwroteCover')]}`}></div>
+
+                <div className={`${altStyles.Findmee} ${altStyles['z-'+state.zOrder.indexOf('Findmee')]}`} onClick={handleFindmeeClick}>{state.fMText}</div>
+                <div className={`${altStyles.FindmeeCover} ${altStyles['z-'+state.zOrder.indexOf('FindmeeCover')]} ${state.isOpen.FindmeeCover?altStyles.open:altStyles.closed}`}>
+                    <div className={altStyles.findMeContentWrapper}>
+                        <div className={altStyles.findMeContent}>
+                            <SocialLink link="https://twitter.com/manglekuo" type="Twitter" tag="@MangleKuo" />
+                            <SocialLink link="https://github.com/ghcpuman902" type="GitHub" tag="ghcpuman902" />
+                            <SocialLink link="https://www.instagram.com/ghcpuman902/" type="Instagram" tag="@ghcpuman902" />
+                            <SocialLink link="https://www.instagram.com/manglekuo/" type="Instagram" tag="@manglekuo" />
+                            <SocialLink link="https://www.behance.net/gallery/65814819/Collection-of-works" type="Behance" tag="mangle-kuo" />
+                            <SocialLink link="https://www.flickr.com/photos/65271177@N06/albums" type="Flickr" tag="Mangle Kuo" />
+                            <SocialLink link="https://www.linkedin.com/in/mangle-k-84198691/" type="LinkedIn" tag="Mangle Kuo" />
+                            <SocialLink link="mailto:manglekuo@gmail.com" type="Email" tag="MangleKuo@gmail.com" />
+                        </div>
+                    </div>
+                </div>
+
             </section>
         </main>
     </>
