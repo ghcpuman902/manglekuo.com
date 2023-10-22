1863050bbd12405dd919833c6876be9776942cd5
MangleKuo <ghcpuman902@gmail.com>
2023-10-22 10:36:49 +0100

    ARTICLE-SEARCH: the http response now set an expire time for the browser to read

--- app/(shadcn)/works/article-search/api/articles-jp/route.ts
+++ app/(shadcn)/works/article-search/api/articles-jp/route.ts
@@ -5,16 +5,32 @@ import { fetchAllJapanArticles } from '../../_utils/fetchRSS';
 export async function GET() {
     const {articles, successfulSources, updateTime} = await fetchAllJapanArticles();
     const env = process.env.NODE_ENV;
-    if(env == "development"){
-        return Response.json({articles, successfulSources, updateTime});
-    }
-    else if (env == "production"){
+
+    var expireDate = new Date(updateTime);
+    expireDate.setHours(expireDate.getHours() + 1); 
+
+    if(env === "development"){
         return Response.json({articles, successfulSources, updateTime}, {
             headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache',
+                'Expires': expireDate.toUTCString(),
+                'Pragma': 'no-cache',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }
+        });
+    }
+    else if (env === "production"){
+        return Response.json({articles, successfulSources, updateTime}, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache',
+                'Expires': expireDate.toUTCString(),
+                'Pragma': 'no-cache',
                 'Access-Control-Allow-Origin': 'https://manglekuo.com',
                 'Access-Control-Allow-Methods': 'GET',
                 'Access-Control-Allow-Headers': 'Content-Type',
-                'Content-Type': 'application/json',
             },
         });
     }
--- app/(shadcn)/works/article-search/api/articles/route.ts
+++ app/(shadcn)/works/article-search/api/articles/route.ts
@@ -5,24 +5,32 @@ import { fetchAllArticles } from '../../_utils/fetchRSS';
 export async function GET() {
     const {articles, successfulSources, updateTime} = await fetchAllArticles();
     const env = process.env.NODE_ENV;
-    if(env == "development"){
+
+    var expireDate = new Date(updateTime);
+    expireDate.setHours(expireDate.getHours() + 1); 
+
+    if(env === "development"){
         return Response.json({articles, successfulSources, updateTime}, {
             headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache',
+                'Expires': expireDate.toUTCString(),
+                'Pragma': 'no-cache',
                 'Access-Control-Allow-Methods': 'GET',
                 'Access-Control-Allow-Headers': 'Content-Type',
-                'Cache-Control': 'no-store',
-                'Content-Type': 'application/json',
             }
         });
     }
-    else if (env == "production"){
+    else if (env === "production"){
         return Response.json({articles, successfulSources, updateTime}, {
             headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache',
+                'Expires': expireDate.toUTCString(),
+                'Pragma': 'no-cache',
                 'Access-Control-Allow-Origin': 'https://manglekuo.com',
                 'Access-Control-Allow-Methods': 'GET',
                 'Access-Control-Allow-Headers': 'Content-Type',
-                'Cache-Control': 'no-store',
-                'Content-Type': 'application/json',
             },
         });
     }
